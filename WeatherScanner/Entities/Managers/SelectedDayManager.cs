8cf806710faa15fadcf713d6beb00f2c861d1161
Kempsi <kempsi1998@gmail.com>
2024-10-15 12:32:30 +0300

    Selected day information panel now displays all needed info, yay!

--- WeatherScanner/Entities/Managers/SelectedDayManager.cs
+++ WeatherScanner/Entities/Managers/SelectedDayManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using WeatherScanner.Entities.Forecast;
+﻿using WeatherScanner.Entities.Forecast;
 using WeatherScanner.Entities.Models;
 using WeatherScanner.Entities.Services;
 using WeatherScanner.UI.ForecastCard;
@@ -70,10 +69,10 @@ namespace WeatherScanner.Entities.Managers
 			SelectedDayPanel.FeelsLike = GetFeelsLike();
 			SelectedDayPanel.WindSpeed = GetWindSpeed();
 			SelectedDayPanel.MyVisibility = GetVisibility();
-			SelectedDayPanel.Timezone = string.Empty;
-			SelectedDayPanel.Humidity = string.Empty;
-			SelectedDayPanel.Sunset = string.Empty;
-			SelectedDayPanel.Sunrise = string.Empty;
+			SelectedDayPanel.Timezone = GetTimeZone();
+			SelectedDayPanel.Humidity = GetHumidity();
+			SelectedDayPanel.Sunset = GetSunset();
+			SelectedDayPanel.Sunrise = GetSunrise();
 
 		}
 
@@ -289,7 +288,93 @@ namespace WeatherScanner.Entities.Managers
 			return "Error";
 		}
 
+		// Returns timezone offset in UTC
+		private string GetTimeZone()
+		{
+			// Offset from the specific city
+			var timezoneOffSet = Response.city.timezone;
+			var offSet = TimeSpan.FromSeconds(timezoneOffSet);
 
+			// Return positive offset
+			if (offSet.Hours > 0)
+			{
+				string offSetString = offSet.ToString().Remove(5);
+				return "Timezone +" + offSetString + " UTC";
+			}
+
+			// Else we return negative offset
+			else
+			{
+				string offSetString = offSet.ToString().Remove(6);
+				return "Timezone " + offSetString + " UTC";
+			}
+
+			
+		}
+
+		// Returns middays humidity from the clicked date
+		private string GetHumidity()
+		{
+			// Get the active card
+			var activeCard = AllCards.FirstOrDefault(card => card.IsActive == true);
+
+			foreach (var item in Response.list)
+			{
+				DateTime date = DateTime.Parse(item.dt_txt);
+
+				// If the active card is the first one
+				// Get the lates humidity
+				if (activeCard == AllCards[0])
+				{
+					if (date.Date == SelectedDate.Date)
+					{
+						return "Humidity " + item.main.humidity.ToString() + "%";
+					}
+				}
+
+				else
+				{
+					// Else we search for matching dates with time of day of 12:00
+					if (date.Date == SelectedDate.Date && date.TimeOfDay.Hours == 12)
+					{
+						return "Humidity " + item.main.humidity.ToString() + "%";
+					}
+
+
+
+				}
+
+
+			}
+
+			return "Error";
+		}
+
+		// Returns sunset in the selected city
+		private string GetSunset()
+		{
+			var unixTime = Response.city.sunset;
+			var offSet = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+
+			var timeZoneOffset = TimeSpan.FromSeconds(Response.city.timezone);
+
+			var cityLocalTime = offSet.ToOffset(timeZoneOffset);
+
+			return "Sunset " + cityLocalTime.ToString("HH:mm");
+		}
+
+		// Returns sunrise in the selected city
+		private string GetSunrise()
+		{
+			var unixTime = Response.city.sunrise;
+			var offSet = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+
+			var timeZoneOffset = TimeSpan.FromSeconds(Response.city.timezone);
+
+			var cityLocalTime = offSet.ToOffset(timeZoneOffset);
+
+			return "Sunrise " + cityLocalTime.ToString("HH:mm");
+		}
 
 		#endregion Data fetching and binding
 
